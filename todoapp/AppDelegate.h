4202188b5844305d093879db1ef9abc984bb6e14
Joey Lin <jvlin@yahoo-inc.com>
2014-01-19 16:20:25 -0800

    Initial Commit

---
+++ todoapp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  todoapp
+//
+//  Created by Joey Lin on 1/19/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
